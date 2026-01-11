44de2ed76c710410439265abc7759d1627a67230
Habib Bouzoffara <habib.bouzaffara@ieee.org>
2026-01-11 15:30:13 +0100

    backend: Add Spring Boot project structure

---
+++ backend/src/main/java/com/roomreservation/room_reservation_api/RoomReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.roomreservation.room_reservation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationApiApplication.class, args);
+	}
+
+}
